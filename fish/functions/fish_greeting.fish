d7436b71c923eba11d96e7029b31d5e0725f5abb
Karan Luciano <karanluciano1@gmail.com>
2021-12-05 23:20:08 -0400

    add fish

---
+++ fish/functions/fish_greeting.fish
@@ -0,0 +1,11 @@
+function fish_greeting
+    echo Bem-vindo Sr. Karan
+    #echo The time is (set_color yellow; date +%T; set_color normal) and this machine is called $hostname
+end
+
+alias ls="lsd"
+
+alias add="sudo pacman -S"
+alias del="sudo pacman -Rn"
+alias update="sudo pacma -Sy"
+alias upgrade="sudo pacman -syu"
